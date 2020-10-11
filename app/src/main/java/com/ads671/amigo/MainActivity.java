fd19110aea3d1753e5151cbe102aa4393b0a8a05
joaotemochko <joaoantonioandre@hotmail.com>
2020-10-11 20:07:16 -0300

    Adicionando arquivos do Projeto amiGO

---
+++ app/src/main/java/com/ads671/amigo/MainActivity.java
@@ -0,0 +1,132 @@
+package com.ads671.amigo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.*;
+import androidx.appcompat.widget.*;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.ads671.amigo.R;
+import com.ads671.amigo.fragment.FeedFragment;
+import com.ads671.amigo.fragment.PerfilFragment;
+import com.ads671.amigo.fragment.PesquisaFragment;
+import com.ads671.amigo.fragment.PostagemFragment;
+import com.ads671.amigo.helper.ConfiguracaoFirebase;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Configura toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("amiGO");
+        setSupportActionBar(toolbar);
+
+        //configurações de objetos
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        //Configurar bottom navigation view
+        configuraBottomNavigationView();
+
+        //Carregar por padrão o feed
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.viewPager, new FeedFragment()).commit();
+
+    }
+
+    /*
+    * Método responsável por criar a BottomNavigation
+    */
+    private void configuraBottomNavigationView(){
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
+
+        //faz configurações iniciais do bottom navigation
+        bottomNavigationViewEx.enableAnimation(true);
+        bottomNavigationViewEx.enableItemShiftingMode(true);
+        bottomNavigationViewEx.enableShiftingMode(true);
+        bottomNavigationViewEx.setTextVisibility(true);
+
+        //Habilitar navegação
+        habilitarNavegacao(bottomNavigationViewEx);
+
+        //Configura item selecionado inicialmente da bottom
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem= menu.getItem(0);
+        menuItem.setChecked(true);
+    }
+
+    /*
+    * Método responsável por tratar eventos de click na BottomNavigation
+    * @param viewEx
+     */
+
+    private void habilitarNavegacao(BottomNavigationViewEx viewEx){
+        viewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                       fragmentTransaction.replace(R.id.viewPager, new FeedFragment()).commit();
+                       return true;
+                    case R.id.ic_pesquisa:
+                        fragmentTransaction.replace(R.id.viewPager, new PesquisaFragment()).commit();
+                        return true;
+                    case R.id.ic_postagem:
+                        fragmentTransaction.replace(R.id.viewPager, new PostagemFragment()).commit();
+                        return true;
+                    case R.id.ic_perfil:
+                        fragmentTransaction.replace(R.id.viewPager, new PerfilFragment()).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_sair:
+                deslogarUsuario();
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario(){
+        try{
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
